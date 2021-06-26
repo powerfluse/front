8906a97658eecb7af54cf42315250b862aada721
linozen <linus@sehn.tech>
2021-06-26 15:03:16 +0200

    some more debugging

--- front/components/grid.js
+++ front/components/grid.js
@@ -7,6 +7,7 @@ import {
   MenuIcon,
   XIcon,
 } from '@heroicons/react/outline'
+import Link from 'next/link'
 
 const support = [
   {
@@ -36,24 +37,28 @@ export default function Grid() {
   return (
     <ul className="px-4 lg:px-8 mt-24 lg:mt-48 grid grid-cols-1 gap-6 lg:grid-cols-3">
       {support.map((s) => (
-        <li
-          key={s.name}
-          className="col-span-1 flex flex-col text-center bg-purple-800 rounded-lg transition-all hover:shadow-xl hover:ring ring-purple-300"
-        >
-          <div className="flex-1 flex flex-col p-8">
-            <div className="h-32 w-32 bg-purple-600 rounded-lg mx-auto flex items-center justify-center">
-              <s.icon className="w-24 h-24 flex-shrink-0 text-gray-300" />
+        <Link href={s.href}>
+          <li
+            key={s.name}
+            className="col-span-1 flex flex-col text-center bg-purple-800 rounded-lg transition-all hover:shadow-xl hover:ring ring-purple-300"
+          >
+            <div className="flex-1 flex flex-col p-8">
+              <div className="h-32 w-32 bg-purple-600 rounded-lg mx-auto flex items-center justify-center">
+                <s.icon className="w-24 h-24 flex-shrink-0 text-gray-300" />
+              </div>
+              <h3 className="mt-6 text-gray-300 text-xl font-medium">
+                {s.name}
+              </h3>
+              <dl className="mt-2 flex-grow flex flex-col justify-between">
+                <dt className="sr-only">s.description</dt>
+                <dd className="text-gray-400 text-md font-source">
+                  {s.description}
+                </dd>
+                <dt className="sr-only">Role</dt>
+              </dl>
             </div>
-            <h3 className="mt-6 text-gray-300 text-xl font-medium">{s.name}</h3>
-            <dl className="mt-2 flex-grow flex flex-col justify-between">
-              <dt className="sr-only">s.description</dt>
-              <dd className="text-gray-400 text-md font-source">
-                {s.description}
-              </dd>
-              <dt className="sr-only">Role</dt>
-            </dl>
-          </div>
-        </li>
+          </li>
+        </Link>
       ))}
     </ul>
   )
