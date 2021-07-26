4c96abcd6ce6e0280903126fccb2f78a63a0a9e3
linozen <linus@sehn.tech>
2021-07-26 14:15:20 +0200

    fix newsletter api

--- front/pages/api/newsletter.js
+++ front/pages/api/newsletter.js
@@ -1,31 +1,70 @@
+import axios from 'axios'
+import nodemailer from 'nodemailer'
+
+// Get environment variables
 const NOCODB_URL = process.env.NOCODB_URL
 const NOCODB_TOKEN = process.env.NOCODB_TOKEN
+const EMAIL_PASS = process.env.EMAIL_PASS
 
 export default function handler(req, res) {
   if (req.method === 'POST') {
-    res.status(200).json(req.body)
     console.log(req.body)
-
-    async function postToNocoDb(data) {
-      // console.log('Token (POST): ', NOCODB_TOKEN)
-      // console.log('Query (POST): ', NOCODB_URL + '/nc/bvpk_9YLS/api/v1/kontakt')
-      const res = await fetch(NOCODB_URL + '/nc/bvpk_9YLS/api/v1/newsletter', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'xc-auth': NOCODB_TOKEN,
-        },
-        body: JSON.stringify(data),
+    // Set axios defaults from environment variables
+    axios.defaults.baseURL = NOCODB_URL
+    axios.defaults.headers.common['xc-auth'] = NOCODB_TOKEN
+    // Send POST request to NocoDB
+    axios
+      .post('/nc/bvpk_9YLS/api/v1/newsletter', req.body)
+      .then((response) => {
+        // console.log(response)
+        res.status(response.status).json(req.body)
       })
-      const json = await res.json()
-      if (json.errors) {
-        console.error(json.errors)
-        throw new Error('Failed to fetch API')
-      }
-      console.log(json)
-      return json
+      .catch((error) => {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data)
+          console.log(error.response.status)
+          console.log(error.response.headers)
+          res.status(error.response.status).json(error.response.data)
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request)
+          res.status(error.request.status).json(error.request)
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message)
+          res.status(400).json(error.message)
+        }
+        console.log(error.config)
+      })
+
+    const transporter = nodemailer.createTransport({
+      port: 465,
+      host: 'smtp.strato.de',
+      auth: {
+        user: 'webmailer@bvpk.org',
+        pass: EMAIL_PASS,
+      },
+      secure: true,
+    })
+
+    const mailData = {
+      from: {
+        name: 'BVPK e.V. Geschäftsstelle',
+        address: 'webmailer@bvpk.org',
+      },
+      to: req.body.nl_email,
+      subject: 'Deine Anmeldung beim BVPK-Newsletter',
+      text: 'Herzlichen Dank für Dein Interesse am BVPK!\n\nWir haben Deine Email in unseren Newsletter-Verteiler aufgenommen. Falls du unsere Emails nicht mehr bekommen möchtest, sende einfach eine kurze Email an info@bvpk.org\n\nMit feurigen Grüßen,\n\nBundesverband Pyrotechnik und Kunstfeuerwerk\nGeschäftsstelle',
     }
-    postToNocoDb(req.body)
+
+    transporter.sendMail(mailData, function (err, info) {
+      if (err) console.log(err)
+      else console.log(info)
+    })
   } else {
     res.status(400).send({ message: 'Only POST requests allowed' })
   }
