d70f052b5f04a6c08e63dfdae809d95da58fdd49
linozen <linus@sehn.tech>
2021-09-25 14:50:33 +0200

    clean-up

--- psql_to_csv.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-CONTAINER="name"
-DB="Db name"
-TABLE="Table Name"
-FILE="file.csv"
-
-sudo docker exec -u postgres "${CONTAINER}" psql -d "${DB}" -c "COPY ${TABLE} TO STDOUT WITH CSV HEADER " > "${FILE}"
