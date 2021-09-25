ed7ab10c9dee500b34e55e40efae7b1fb166963f
linozen <linus@sehn.tech>
2021-09-25 23:58:05 +0200

    minor changes to export

--- export/to_csv.py
+++ export/to_csv.py
@@ -103,6 +103,9 @@ df_firmenmitglieder = df_firmenmitglieder.loc[
 df_firmenmitglieder["f_iban"] = df_firmenmitglieder["f_iban"].apply(
     lambda x: "****" + x[-4:]
 )
+df_firmenmitglieder.loc[:, ["zahlungsrhythmus"]] = df_firmenmitglieder[
+    "zahlungsrhythmus"
+].apply(lambda x: x.lower() if x else None)
 df_firmenmitglieder["sign_up"] = "bvpk.org/mitglied-werden-firma"
 df_firmenmitglieder.to_csv("./output/cr_firmenmitglieder.csv", index=False)
 
