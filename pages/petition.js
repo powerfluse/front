b770557fa411b2025878f3c23b3c96d4919e026c
linozen <linus@sehn.tech>
2022-12-10 09:47:23 +0100

    Link `petition` to CMS

--- pages/petition.js
+++ pages/petition.js
@@ -1,12 +1,43 @@
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
+// import { useEffect } from 'react'
+// import { useRouter } from 'next/router'
 
-export default function Page() {
-  const router = useRouter()
+import parse from 'html-react-parser'
+import getFromDirectus from '../lib/directus'
+import Head from '../components/head'
+import NavBar from '../components/navbar'
+import Newsletter from '../components/newsletter'
+import Footer from '../components/footer'
 
-  useEffect(() => {
-    router.push('https://bvpk.org')
-  })
+// export default function Page() {
+//   const router = useRouter()
+//   useEffect(() => {
+//     router.push('https://bvpk.org')
+//   })
+//   return <p>Redirecting...</p>
+// }
 
-  return <p>Redirecting...</p>
+export default function (props) {
+  return (
+    <>
+      <Head />
+      <NavBar />
+      <div className="min-h-screen">
+        <div className="prose-bvpk pt-32 pb-12 mx-auto">
+          {parse(props.dataPage.text)}
+        </div>
+      </div>
+      <Newsletter />
+      <Footer />
+    </>
+  )
+}
+
+export async function getStaticProps() {
+  const dataPage = await getFromDirectus('/items/petition_page')
+  return {
+    props: {
+      dataPage,
+    },
+    revalidate: 60,
+  }
 }
