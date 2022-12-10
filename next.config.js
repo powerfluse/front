ade759a76a3038ce9a05e29c047adb7ad6776ad7
linozen <linus@sehn.tech>
2022-12-10 10:49:27 +0100

    Add redirect from `/novelle-sprengstoffgesetz`

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/novelle-sprengstoffgesetz',
+        destination: '/aktuelles/positionspapier-silvester',
+        permanent: true,
+      },
+    ]
+  },
   images: {
     domains: ['localhost', 'cms.bvpk.org'],
   },
