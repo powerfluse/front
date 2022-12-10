08b4db56bb28550ea8157ec6280986a332e6c0d6
linozen <linus@sehn.tech>
2022-12-10 10:24:47 +0100

    Fix some hydration errors

--- pages/aktuelles/[slug].js
+++ pages/aktuelles/[slug].js
@@ -40,24 +40,23 @@ export default function Post(props) {
                       height={1376}
                     />
                   </div>
-                  <figcaption className="mt-3 flex text-sm text-gray-500">
-                    <CameraIcon
-                      className="flex-none w-5 h-5 text-gray-400"
-                      aria-hidden="true"
-                    />
-                    <span className="ml-2">
-                      Bild von{' '}
-                      <a
-                        className={`${
-                          props.imagelink ? 'underline hover:text-bvpk-300' : ''
-                        }`}
-                      >
-                        <a href={props.imagelink || '/impressum'}>
+                  {props.imageattribution && (
+                    <figcaption className="mt-3 flex text-sm text-gray-500">
+                      <CameraIcon
+                        className="flex-none w-5 h-5 text-gray-400"
+                        aria-hidden="true"
+                      />
+                      <span className="ml-2">
+                        Bild von{' '}
+                        <a
+                          className="underline font-bold decoration-bvpk-300"
+                          href={props.imagelink || '/impressum'}
+                        >
                           {props.imageattribution}
                         </a>
-                      </a>
-                    </span>
-                  </figcaption>
+                      </span>
+                    </figcaption>
+                  )}
                 </figure>
               </div>
             </div>
