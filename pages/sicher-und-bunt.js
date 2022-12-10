0325e17f769a71e8c8485ef13a0dcb5d913c73ed
linozen <linus@sehn.tech>
2022-12-10 11:23:57 +0100

    Some modifications for `sicher-und-bunt`

--- pages/sicher-und-bunt.js
+++ pages/sicher-und-bunt.js
@@ -11,7 +11,7 @@ export default function SicherUndBunt(props) {
       <Head />
       <NavBar />
       <div className="min-h-screen">
-        <div className="prose-bvpk-wide text-gray-700 pt-32 pb-12 mx-auto">
+        <div className="prose-bvpk-wide text-gray-700 pt-32 pb-12 mx-auto prose-img:shadow-none prose-img:text-center">
           {parse(props.dataPage.text)}
         </div>
       </div>
