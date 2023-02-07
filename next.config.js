079e37ed50da265f9decc01292567bcbac4deb4b
linozen <linus@sehn.tech>
2023-02-07 14:52:48 +0100

    Ignore linting during `next build` (for now)

--- next.config.js
+++ next.config.js
@@ -11,4 +11,9 @@ module.exports = {
     images: {
         domains: ['localhost', 'cms.bvpk.org'],
     },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
 }
